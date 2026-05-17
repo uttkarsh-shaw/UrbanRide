914f90072ebe7db25cbd558aacc881f95a52da0f
uttkarsh-shaw <manjushaw101@gmail.com>
2026-05-18 03:58:54 +0530

    connected live backend

--- Frontend/uber_frontend/src/api/axios.js
+++ Frontend/uber_frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://urbanride-z7ug.onrender.com"
+  baseURL: import.meta.env.VITE_API_URL || "https://urbanride-z7ug.onrender.com/api"
 });
 
 API.interceptors.request.use((config) => {
